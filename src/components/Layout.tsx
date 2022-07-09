5055c7c54b769f96473b4f4e80960459af8f0ef8
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-07-09 11:04:28 +0200

    :hammer: chore: load fonts locally

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -45,12 +45,6 @@ export function Layout({ children }: LayoutProps) {
     <>
       <Head>
         <title>Earnings Tracker</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <div className="flex">
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
       fontFamily: {
         sans: 'Raleway, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-        serif: 'Roboto Slab, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+        serif: 'RobotoSlab, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
       },
     },
   },
