1302763f0dda90f1f6cf83b6c0eafe70c5f7f591
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-07-14 10:42:38 +0200

    fix: failing ci

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,10 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { prisma } from "utils/prisma";
 
+if (!process.env.GITHUB_ID || !process.env.SECRET || !process.env.GITHUB_SECRET) {
+  throw new Error("Must set GITHUB_ID, SECRET, and GITHUB_SECRET in environment");
+}
+
 export const authOptions: NextAuthOptions = {
   secret: process.env.SECRET,
   providers: [
