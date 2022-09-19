47fff5ed699699638ea53c101eef9a20434d88a7
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-09-19 17:42:25 +0200

    :tada: feat: upgrade to tRPC v10 beta

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,4 +1,16 @@
 import { createReactQueryHooks } from "@trpc/react";
+import { TRPCError } from "@trpc/server";
 import type { AppRouter } from "server/routers/_app";
+import { t } from "server/trpc";
 
 export const trpc = createReactQueryHooks<AppRouter>();
+
+export const isAuth = t.procedure.use(
+  t.middleware(({ next, ctx }) => {
+    if (!ctx.session || !ctx.dbUser) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    return next();
+  }),
+);
